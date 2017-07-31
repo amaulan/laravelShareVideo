d0a72a0a8a65eee6cfe87f4c1f9461444a6908e0
rizkimuhammad1305 <rizki1305indosystem@gmail.com>
2017-07-31 14:14:28 +0700

    category

--- app/Category.php
+++ app/Category.php
@@ -6,8 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
+
+
    public function courses()
     {
         return $this->belongstoMany(Course::class);
     }
+
+	public $timestamps = false;
+
+	protected $fillable = [
+
+   'is_enable',
+
+   ];
+
 }
